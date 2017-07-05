c38f00fe0b5dd31e45a41100132db6979f9f9a93
IceHe.xyz <icehe.xyz@qq.com>
2017-07-05 22:43:46 +0800

    Site updated: 2017-07-05 22:43:45

---
+++ js/src/schemes/pisces.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
